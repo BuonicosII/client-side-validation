f104b011b2c5646a637d93a0e2401341f1efb4a7
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-08-29 20:53:04 +0200

    Add inline validation to email input field
    As stated. Also add montserrat as custom font.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import './style.css';
 
-let email = document.querySelector("#email");
-email.addEventListener("blur", ()=>{
-    if (email.validity.typeMismatch) {
-        email.nextElementSibling.innerHTML = "Insert a valid mail"
-    } else {
 
+//email input field functions
+let email = document.querySelector("#email");
+
+email.addEventListener("blur", ()=>{
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) && email.value.length > 0 ) {
+        email.nextElementSibling.innerHTML = "Insert a valid mail!"
+        email.classList.add("invalid");
+    } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+        email.classList.add("valid");
+    }
+});
+
+email.addEventListener("focusin", ()=>{
+    if (email.hasAttribute("class", "invalid")) {
+        email.classList.remove("invalid");
+        email.nextElementSibling.innerHTML = ""
+    }
+    if (email.hasAttribute("class", "valid")) {
+        email.classList.remove("valid");
     }
 })
 
